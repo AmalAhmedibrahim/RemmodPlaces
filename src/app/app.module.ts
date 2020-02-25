9c00efe2509862b24141a575c98fb9e096873ef6
Amal <amal.ahmed@cis.asu.edu.eg>
2020-02-25 22:58:38 +0200

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from "./layout/header/header.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
@@ -5,12 +6,21 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReviewsComponent } from "./Reviews/Reviews.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { SharedModule } from "./shared/shared.module";
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
-  declarations: [AppComponent, ReviewsComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, ButtonsModule.forRoot()],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    LayoutModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
